c810eac87ceaf0d5496dd1f1187319af3864565c
Gina <ginamantonini@gmail.com>
2018-03-05 20:13:07 -0600

    added main bangazon ordering menu

--- bangazoncli/Program.cs
+++ bangazoncli/Program.cs
@@ -10,6 +10,44 @@ namespace bangazoncli
     {
         static void Main(string[] args)
         {
+            var db = SetupNewApp();
+
+            var run = true;
+            while (run)
+            {
+                ConsoleKeyInfo userInput = MainMenu();
+            }
+        }
+
+        static DatabaseContext SetupNewApp()
+        {
+            Console.Title = "Bangazon Command Line Ordering System";
+            var cSharp = 554;
+            var db = new DatabaseContext(tone: cSharp);
+            return db;
+        }
+
+        static ConsoleKeyInfo MainMenu()
+        {
+            View mainMenu = new View()
+                .AddMenuOption("Create a customer account")
+                .AddMenuOption("Choose active customer")
+                .AddMenuOption("Create a payment option")
+                .AddMenuOption("Add product to sell")
+                .AddMenuOption("Add product to shopping cart")
+                .AddMenuOption("Complete an order")
+                .AddMenuOption("Remove customer product")
+                .AddMenuOption("Update product information")
+                .AddMenuOption("Show stale products")
+                .AddMenuOption("Show customer revenue report")
+                .AddMenuOption("Show overall product popularity")
+                .AddMenuOption("Leave Bangazon!");
+
+            Console.Write(mainMenu.GetFullMenu());
+            ConsoleKeyInfo userOption = Console.ReadKey();
+            return userOption;
+
         }
     }
 }
+
