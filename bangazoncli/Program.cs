14b4e20c1426c3a11d41bc140e52b3f250c2d1fc
Gina <ginamantonini@gmail.com>
2018-03-05 20:19:53 -0600

    commented out menu options we are not working on currently

--- bangazoncli/Program.cs
+++ bangazoncli/Program.cs
@@ -32,16 +32,16 @@ namespace bangazoncli
             View mainMenu = new View()
                 .AddMenuOption("Create a customer account")
                 .AddMenuOption("Choose active customer")
-                .AddMenuOption("Create a payment option")
-                .AddMenuOption("Add product to sell")
-                .AddMenuOption("Add product to shopping cart")
-                .AddMenuOption("Complete an order")
-                .AddMenuOption("Remove customer product")
-                .AddMenuOption("Update product information")
-                .AddMenuOption("Show stale products")
-                .AddMenuOption("Show customer revenue report")
-                .AddMenuOption("Show overall product popularity")
-                .AddMenuOption("Leave Bangazon!");
+                //.AddMenuOption("Create a payment option")
+                //.AddMenuOption("Add product to sell")
+                //.AddMenuOption("Add product to shopping cart")
+                //.AddMenuOption("Complete an order")
+                //.AddMenuOption("Remove customer product")
+                //.AddMenuOption("Update product information")
+                //.AddMenuOption("Show stale products")
+                //.AddMenuOption("Show customer revenue report")
+                //.AddMenuOption("Show overall product popularity")
+                //.AddMenuOption("Leave Bangazon!");
 
             Console.Write(mainMenu.GetFullMenu());
             ConsoleKeyInfo userOption = Console.ReadKey();
