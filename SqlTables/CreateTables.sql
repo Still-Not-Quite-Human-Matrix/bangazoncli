23a0e4cd72d72d3c67e46a1804e0cf0be401cd43
Ken Lewis <kenneth.g.lewis2@gmail.com>
2018-03-17 10:26:08 -0500

    Update CreateTables.sql

--- SqlTables/CreateTables.sql
+++ SqlTables/CreateTables.sql
@@ -42,6 +42,7 @@ CREATE TABLE [DBO].[Payment]
   [PaymentID] INT NOT NULL IDENTITY (1, 1),
   [PaymentType] VARCHAR(30) NOT NULL,
   [PaymentAccountNum] INT NOT NULL,
+  [CusID] INT NOT NULL
   CONSTRAINT [PK_Payment] PRIMARY KEY CLUSTERED ([PaymentID])
 );
 GO
@@ -50,6 +51,9 @@ CREATE TABLE [dbo].[Product]
   [ProductID] INT NOT NULL IDENTITY (1, 1),
   [Name] VARCHAR(30) NOT NULL,
   [Price] MONEY NOT NULL,
+  [Owner] INT NOT NULL,
+  [Count] [int] NOT NULL,
+  [Description] [nvarchar](200) NULL,
   CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED ([ProductID])
 );
 GO
@@ -82,7 +86,7 @@ ALTER TABLE [dbo].[Order] ADD CONSTRAINT [FK_CustomerID]
 GO
 CREATE INDEX [IFK_OrderCustomerID] ON [dbo].[Customer] ([CustomerID]);
 GO
-ALTER TABLE [dbo].[OrderItem] ADD CONSTRAINT [FK_ItemProductID]
+ALTER TABLE [dbo].[OrderItem] ADD CONSTRAINT [FK_ProductID]
     FOREIGN KEY ([ProductID]) REFERENCES [dbo].[Product] ([ProductID]) ON DELETE NO ACTION ON UPDATE NO ACTION;
 GO
 CREATE INDEX [IFK_OrderItemProductID] ON [dbo].[Product] ([ProductId]);
@@ -91,4 +95,14 @@ ALTER TABLE [dbo].[OrderItem] ADD CONSTRAINT [FK_OrderID]
     FOREIGN KEY ([OrderID]) REFERENCES [dbo].[Order] ([OrderID]) ON DELETE NO ACTION ON UPDATE NO ACTION;
 GO
 CREATE INDEX [IFK_OrderOrderID] ON [dbo].[Order] ([OrderId]);
+GO
+ALTER TABLE [dbo].[Product] ADD CONSTRAINT [FK_Owner]
+    FOREIGN KEY ([Owner]) REFERENCES [dbo].[Customer] ([CustomerID]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+CREATE INDEX [IFK_ProductOwnder] ON [dbo].[Customer] ([CustomerID]);
+GO
+ALTER TABLE [dbo].[Payment] ADD CONSTRAINT [FK_CusID]
+    FOREIGN KEY ([CusID]) REFERENCES [dbo].[Customer] ([CustomerID]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+CREATE INDEX [IFK_PaymentCusID] ON [dbo].[Customer] ([CustomerID]);
 GO
